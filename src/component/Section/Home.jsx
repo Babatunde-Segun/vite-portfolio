69f3bf24354b4443c88ea70c998f2cbf9678700c
Babatunde-Segun <officiallyvincify@gmail.com>
2024-06-03 18:39:00 +0100

    home almost done

--- src/component/Section/Home.jsx
+++ src/component/Section/Home.jsx
@@ -4,6 +4,7 @@ import Tag from "../Tag/Tag";
 import Button from "../Button/Button";
 import "./Home.css";
 import ProjectsBtn from "../ProjectsBtn";
+import CountUp from "react-countup";
 
 function Home() {
   return (
@@ -18,6 +19,39 @@ function Home() {
             <h1 className="home-primary-header">
               I'm Babatunde Segun <br />A Software <span>Developer</span>
             </h1>
+          </div>
+
+          {/* Countup Section */}
+          <div className="my-[3rem] text-[1.5rem] sm:text-3xl flex gap-[5rem]  flex-wrap">
+            <div className="  text-[orangered] ">
+              <div className="flex gap-1">
+                <CountUp
+                  start={0}
+                  end={2}
+                  delay={3}
+                  duration={5}
+                  className="text-[3rem] font-[700] mb-[1rem]"
+                />
+                +
+              </div>
+              <p className="text-[1.3rem] sm:text-xl text-[white]">
+                Years of Experience
+              </p>
+            </div>
+
+            <div className=" lg:text-3xl text-[orangered]">
+              <div className="flex gap-1">
+                <CountUp
+                  start={0}
+                  end={20}
+                  delay={1}
+                  duration={5}
+                  className="text-[3rem] font-[700] mb-[1rem]"
+                />
+                +
+              </div>
+              <p className="text-[1.3rem] sm:text-xl text-[white]">Projects</p>
+            </div>
           </div>
 
           <div className="home-btn-container">
