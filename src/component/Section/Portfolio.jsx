d581a649f578213ba77bbeafa5d49ff42cd73f9c
Babatunde-Segun <officiallyvincify@gmail.com>
2024-06-05 17:33:36 +0100

    day steadily bomb2

--- src/component/Section/Portfolio.jsx
+++ src/component/Section/Portfolio.jsx
@@ -4,23 +4,30 @@ import thumb1 from "../../assets/thumb1.jpg";
 import thumb2 from "../../assets/thumb2.jpg";
 import thumb3 from "../../assets/thumb3.jpg";
 import thumb4 from "../../assets/thumb4.jpg";
+import nextfoodfullscreen from "../../assets/nextfoodfullscreen.png";
 import "./Portfolio.css";
+import { Link } from "react-router-dom";
 
 const portfolioArray = [
   {
     name: "title 1",
     src: thumb1,
-    tags: ["node", "react"],
+    tags: ["node", "React"],
   },
   {
     name: "title 2",
     src: thumb2,
-    tags: ["node", "javascript"],
+    tags: ["node", "JavaScript"],
   },
   {
     name: "title 3",
     src: thumb3,
-    tags: ["react", "nextjs", "typescript"],
+    tags: ["react", "NextJS", "typescript"],
+  },
+  {
+    name: "title 4",
+    src: nextfoodfullscreen,
+    tags: ["NextJS"],
   },
 ];
 
@@ -38,15 +45,15 @@ function Portfolio() {
         {portfolioArray.map((items) => (
           <div className="portfolio-img-container">
             <img src={items.src} alt={items.name} className="portfolio-img" />
-            {items.tags.length === 0
-              ? ""
-              : items.tags.map((tag) => (
-                  <div className="portfolio-details-container">
-                    <div className="portfolio-details-subcontainer">
+            <div className="portfolio-details-container">
+              <div className="portfolio-details-subcontainer">
+                {items.tags.length === 0
+                  ? ""
+                  : items.tags.map((tag) => (
                       <p className="portfolio-img-details">{tag}</p>
-                    </div>
-                  </div>
-                ))}
+                    ))}
+              </div>
+            </div>
           </div>
         ))}
       </div>
