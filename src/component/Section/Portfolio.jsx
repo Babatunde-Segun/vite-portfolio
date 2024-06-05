33d3929fd0753e5aadce7e842497268bfc88ba13
Babatunde-Segun <officiallyvincify@gmail.com>
2024-06-05 16:02:46 +0100

    day bomb

--- src/component/Section/Portfolio.jsx
+++ src/component/Section/Portfolio.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import Tag from "../Tag/Tag";
+import thumb1 from "../../assets/thumb1.jpg";
+import thumb2 from "../../assets/thumb2.jpg";
+import thumb3 from "../../assets/thumb3.jpg";
+import thumb4 from "../../assets/thumb4.jpg";
 
 const portfolioArray = [
   {
-    src: "",
-    tag: ["node", "react"],
+    name: "title 1",
+    src: thumb1,
+    tags: ["node", "react"],
   },
   {
-    src: "",
-    tag: ["node", "javascript"],
+    name: "title 2",
+    src: thumb2,
+    tags: ["node", "javascript"],
   },
   {
-    src: "",
-    tag: ["react", "nextjs", "typescript"],
+    name: "title 3",
+    src: thumb3,
+    tags: ["react", "nextjs", "typescript"],
   },
 ];
 
@@ -26,7 +33,18 @@ function Portfolio() {
         Featured <span>Project</span>
       </h2>
 
-      <div className="portfolio-array"></div>
+      <div className="portfolio-array">
+        {portfolioArray.map((items) => (
+          <div className="portfolio-img-container">
+            <img src={items.src} alt={items.name} className="portfolio-img" />
+            {items.tags.length === 0
+              ? ""
+              : items.tags.map((tag) => (
+                  <p className="portfolio-img-details">{tag}</p>
+                ))}
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
