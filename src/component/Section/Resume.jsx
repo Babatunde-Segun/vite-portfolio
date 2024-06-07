946e304eff3c0ca5c20fe0c6649a7f3098a96ab0
Babatunde-Segun <officiallyvincify@gmail.com>
2024-06-07 17:56:10 +0100

    day 2, bomb

--- src/component/Section/Resume.jsx
+++ src/component/Section/Resume.jsx
@@ -5,6 +5,7 @@ import "./Resume.css";
 function Resume() {
   return (
     <section id="resume">
+      {/* Tag */}
       <Tag>Resume</Tag>
 
       <h2 className="secondary-heading resume-secondary-heading">
@@ -12,6 +13,17 @@ function Resume() {
       </h2>
 
       <h3 className="resume-tertiary-heading">My Education</h3>
+
+      <div className="resume-flex-container">
+        <div className="resume-flex-container-date">2023</div>
+
+        <div className="resume-flex-container-course">
+          <h3>B.Sc Physics</h3>
+          <p>University of Ibadan, Ibadan, Nigeria</p>
+        </div>
+
+        <div className="resume-flex-container-details"></div>
+      </div>
     </section>
   );
 }
