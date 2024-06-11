ef8bce4401e57a3278837e9433987312ff1fd054
Babatunde-Segun <officiallyvincify@gmail.com>
2024-06-11 01:03:32 +0100

    progress

--- src/component/Countup.jsx
+++ src/component/Countup.jsx
@@ -3,7 +3,7 @@ import CountUp from "react-countup";
 
 function CountupComponent() {
   return (
-    <div className="my-[3rem] text-[1.5rem] sm:text-3xl flex gap-[5rem]  flex-wrap">
+    <div className="my-[3rem] text-[1.5rem] sm:text-3xl flex gap-[2rem] sm:gap-[5rem]  flex-wrap">
       <div className="  text-[orangered] ">
         <div className="flex gap-1">
           <CountUp
@@ -11,11 +11,11 @@ function CountupComponent() {
             end={2}
             delay={3}
             duration={5}
-            className="text-[3rem] font-[700] mb-[1rem]"
+            className="text-[3rem] font-[700] mb-[0.2rem] sm:mb-[1rem]"
           />
           +
         </div>
-        <p className="text-[1.3rem] sm:text-xl text-[white]">
+        <p className="text-[1rem]  sm:text-xl text-[white]">
           Years of Experience
         </p>
       </div>
@@ -27,11 +27,11 @@ function CountupComponent() {
             end={20}
             delay={1}
             duration={5}
-            className="text-[3rem] font-[700] mb-[1rem]"
+            className="text-[3rem] font-[700] mb-[0.2rem] sm:mb-[1rem]"
           />
           +
         </div>
-        <p className="text-[1.3rem] sm:text-xl text-[white]">Projects</p>
+        <p className="text-[1rem] sm:text-xl text-[white]">Projects</p>
       </div>
     </div>
   );
