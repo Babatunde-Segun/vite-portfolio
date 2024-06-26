d46e15ce261e0653d695e959586fae9416c162d7
Babatunde-Segun <officiallyvincify@gmail.com>
2024-06-26 23:01:03 +0100

    projectimg added

--- src/component/ProjectsBtn.jsx
+++ src/component/ProjectsBtn.jsx
@@ -6,15 +6,22 @@
 import { Link } from "react-router-dom";
 // import bgStar from "../assets/circleStar.svg";
 import roundedText from "../assets/rounded-text.png";
+import projectText from "../assets/project.png";
 
 // icons
 import { HiArrowRight } from "react-icons/hi2";
 import { HiArrowLongDown } from "react-icons/hi2";
+import { useContext } from "react";
+import { Box, IconButton, useTheme } from "@mui/material";
+import { ColorModeContext, tokens } from "../theme";
 {
   /* <HiArrowLongDown />; needed for down btn */
 }
 
 const ProjectsBtn = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
   return (
     <a
       href="#portfolio"
@@ -23,6 +30,16 @@ const ProjectsBtn = () => {
       }}
     >
       <div className="relative w-[30px] h-[38px] flex justify-center items-center  bg-cover bg-center bg-no-repeat group">
+        {theme.palette.mode === "light" && (
+          <>
+            <img
+              src={projectText}
+              alt=""
+              className="projectBtn  max-w-[100px] max-h-[108px]"
+            />
+            <HiArrowLongDown className="absolute text-4xl group-hover:translate-y-2 transition-all duration-300 ease" />
+          </>
+        )}
         <img
           src={roundedText}
           alt=""
