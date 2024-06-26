9cbe5c25e1b9f4913a3d22917857d4673d3ce079
Babatunde-Segun <officiallyvincify@gmail.com>
2024-06-26 01:56:35 +0100

    adding loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import SideBar from "./component/SideBarComponent";
@@ -9,27 +9,40 @@ import About from "./component/Section/About";
 import Resume from "./component/Section/Resume";
 import Contact from "./component/Section/Contact";
 import Nav from "./component/Nav";
+import Loader from "./component/loader/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
   const [theme, colorMode] = useMode();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-
-        <div className="app">
-          {/* <Navbar /> */}
-          <Nav />
-          <SideBar />
-          <main className="main">
-            <Home />
-            <Portfolio />
-            <About />
-            <Resume />
-            <Contact />
-          </main>
-        </div>
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className="app">
+            {/* <Navbar /> */}
+            <Nav />
+            <SideBar />
+            <main className="main">
+              <Home />
+              <Portfolio />
+              <About />
+              <Resume />
+              <Contact />
+              <Loader />
+            </main>
+          </div>
+        )}
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
